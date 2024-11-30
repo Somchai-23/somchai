36c7a774526d146ee572f42a50e8ab5d93a348f4
Somchai-23 <somchai171088@gmail.com>
2024-11-30 12:20:49 +0700

    Profile

---
+++ app/routes/profile.tsx
@@ -0,0 +1,31 @@
+function MyProfile(){
+    return (
+        <div className="p-5 m-5 border-8">
+            <h1>
+                My Profile
+            </h1>
+            <strong>
+                Name: somchai Charoenrum 
+            </strong>
+            <strong>
+                026740491604-6 
+                กากา
+            </strong>
+            <MyEducateion/>
+        </div>
+    )
+}
+
+function MyEducateion(){
+    return (
+        <div>
+            <ul>
+                <li>Information Technology, RMUTTO, 256-Present</li>
+                <li>03/05/2003</li>
+            </ul>
+        </div>
+    );
+
+}
+
+export default  MyProfile;
